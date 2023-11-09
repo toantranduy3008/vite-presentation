0da526080fa984c2e86ad2d710be3438fdaaae19
toantranduy3008 <39187689+toantranduy3008@users.noreply.github.com>
2023-11-09 11:38:58 +0700

    Update tailwind.config.js
    Bổ sung 2 kích thước màn hình

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ export default {
       fontFamily: {
         sans: 'Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji'
       },
-
+      screens: {
+        'xs': '300px',
+        '3xl': '2000px',
+      },
     },
   },
   plugins: [],
